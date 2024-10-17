a98af0c3ba8f03f37c719fda2f7304a069a51231
Mohansai <bandarumohansai@gmail.com>
2024-10-17 17:30:48 +0530

    checking docker_installation_script

--- docker_installation.sh
+++ docker_installation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 user=$(id -u)
-echo $?
 
 
 if [ $user -ne 0 ]
@@ -10,5 +9,12 @@ echo "you are not having root access,Please switch to root access"
 exit -1
 else
 echo "You have root access hence installing the docker"
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+
 fi
+
 
