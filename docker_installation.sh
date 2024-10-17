e50ed1118ff6c02d12f820f2b2711b8350bdfaa2
Mohansai <bandarumohansai@gmail.com>
2024-10-17 17:25:33 +0530

    checking docker_installation_script

---
+++ docker_installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+user=${id -u}
+echo $?
+
+
+if [ $user -ne 0 ]
+then
+echo "you are not having root access,Please switch to root access"
+exit -1
+else
+echo "You have root access hence installing the docker"
+fi
+
